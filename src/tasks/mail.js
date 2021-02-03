8343d6e1b6e0d460bd538a5d30b0a073fcf41e09
Changhun Lee <ch.lee@danbicorp.com>
2021-02-03 18:36:06 +0900

    fix(gulp tasks): gulp task 경로 수정

--- src/tasks/mail.js
+++ src/tasks/mail.js
@@ -4,7 +4,7 @@ const { spawnSync } = require("child_process");
 task("Send Payment Approved", async function () {
   const sendPaymentApproved = await spawnSync(
     "node",
-    ["src/index.payment_approved.js"],
+    ["src/utils/payment_approved.js"],
     { stdio: "inherit" }
   );
   return sendPaymentApproved;
@@ -13,7 +13,7 @@ task("Send Payment Approved", async function () {
 task("Send Payment Denied", async function () {
   const sendPaymentDenied = await spawnSync(
     "node",
-    ["src/index.payment_denied.js"],
+    ["src/utils/payment_denied.js"],
     { stdio: "inherit" }
   );
   return sendPaymentDenied;
@@ -22,7 +22,7 @@ task("Send Payment Denied", async function () {
 task("Send Advertisement Approved", async function () {
   const sendAdvertisementApproved = await spawnSync(
     "node",
-    ["src/index.advertisement_approved.js"],
+    ["src/utils/advertisement_approved.js"],
     { stdio: "inherit" }
   );
   return sendAdvertisementApproved;
@@ -31,7 +31,7 @@ task("Send Advertisement Approved", async function () {
 task("Send Advertisement Denied", async function () {
   const sendAdvertisementDenied = await spawnSync(
     "node",
-    ["src/index.advertisement_denied.js"],
+    ["src/utils/advertisement_denied.js"],
     { stdio: "inherit" }
   );
   return sendAdvertisementDenied;
