c25268bbc73808944d6a2795a43a8a1428bfdf4a
Changhun Lee <ch.lee@danbicorp.com>
2021-02-04 18:09:03 +0900

    fix(rename title): payment_denied.html > 결제 오류 > 결제 실패

---
+++ src/index.js
@@ -0,0 +1,4 @@
+exports.payment_approved = require("./utils/payment_approved");
+exports.payment_denied = require("./utils/payment_denied");
+exports.advertisement_approved = require("./utils/advertisement_approved");
+exports.advertisement_denied = require("/utils/advertisement_denied");
