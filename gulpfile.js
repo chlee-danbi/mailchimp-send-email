2c17117387914684493962839e67d57bfa0d4f30
Changhun Lee <ch.lee@danbicorp.com>
2021-02-09 19:58:59 +0900

    feat(add mailjet gulp series): mailjet gulp serires 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,43 @@
 const { series } = require("gulp");
 require("./src/tasks");
 
-// => 제작된 모든 이메일 템플릿 전송
-exports.sendAllEmailTemplate = series(
-  "Send Payment Approved",
-  "Send Payment Denied",
-  "Send Advertisement Approved",
-  "Send Advertisement Denied"
+// => [Mandrill] 제작된 모든 이메일 템플릿 전송
+exports.sendAllEmailTemplateMandrill = series(
+  "Mandrill Send Payment Approved",
+  "Mandrill Send Payment Denied",
+  "Mandrill Send Advertisement Approved",
+  "Mandrill Send Advertisement Denied"
 );
 
-// => 제작된 모든 결제 이메일 템플릿 전송
-exports.sendPaymentEmailTemplate = series(
-  "Send Payment Approved",
-  "Send Payment Denied"
+// => [Mandrill] 제작된 모든 결제 이메일 템플릿 전송
+exports.sendPaymentEmailTemplateMandrill = series(
+  "Mandrill Send Payment Approved",
+  "Mandrill Send Payment Denied"
 );
 
-// => 제작된 모든 결제 이메일 템플릿 전송
-exports.sendAdvertisementEmailTemplate = series(
-  "Send Advertisement Approved",
-  "Send Advertisement Denied"
+// => [Mandrill] 제작된 모든 결제 이메일 템플릿 전송
+exports.sendAdvertisementEmailTemplateMandrill = series(
+  "Mandrill Send Advertisement Approved",
+  "Mandrill Send Advertisement Denied"
+);
+
+
+// => [Mailjet] 제작된 모든 이메일 템플릿 전송
+exports.sendAllEmailTemplateMailjet = series(
+  "Mailjet Send Payment Approved",
+  "Mailjet Send Payment Denied",
+  "Mailjet Send Advertisement Approved",
+  "Mailjet Send Advertisement Denied"
+);
+
+// => [Mailjet] 제작된 모든 결제 이메일 템플릿 전송
+exports.sendPaymentEmailTemplateMailjet = series(
+  "Mailjet Send Payment Approved",
+  "Mailjet Send Payment Denied"
+);
+
+// => [Mailjet] 제작된 모든 결제 이메일 템플릿 전송
+exports.sendAdvertisementEmailTemplateMailjet = series(
+  "Mailjet Send Advertisement Approved",
+  "Mailjet Send Advertisement Denied"
 );
