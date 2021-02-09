4c698387307d026af7274f2e0563fa201d4ee986
Changhun Lee <ch.lee@danbicorp.com>
2021-02-10 08:34:22 +0900

    fix(API KEY): API_KEY 수정

--- src/utils/mailjet/payment_approved.js
+++ src/utils/mailjet/payment_approved.js
@@ -10,10 +10,7 @@ require("dotenv").config();
 const PUBLIC_API_KEY = process.env.MAILJET_API_KEY;
 const PRIVATE_API_KEY = process.env.MAILJET_SECRET_KEY;
 
-const mailjet = mail.connect(
-  "7929463af6e5c92b7b185511b052b461",
-  "afc742d5ff121f3227ee6245d188976a"
-);
+const mailjet = mail.connect(PUBLIC_API_KEY || "", PRIVATE_API_KEY || "");
 
 const request = mailjet.post("send", { version: "v3.1" }).request({
   Messages: [
